f72b9c434507bc267f5bae31926c9d969bfbce7c
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-05-08 20:35:25 +0200

    Update AutoMapperProfile.cs

--- ComputerStore.API/Mappings/AutoMapperProfile.cs
+++ ComputerStore.API/Mappings/AutoMapperProfile.cs
@@ -12,19 +12,18 @@ namespace ComputerStore.API.Mappings
             CreateMap<Product, ProductDto>()
                 .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.Categories.Select(c => c.Name)))
                 .ReverseMap()
-                .ForMember(dest => dest.Categories, opt => opt.Ignore()); // Optional: Handle manually if needed
-
+                .ForMember(dest => dest.Categories, opt => opt.Ignore());
             // Map Product <-> StockDto
             CreateMap<Product, StockDto>()
                 .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.Categories.Select(c => c.Name)))
                 .ReverseMap()
-                .ForMember(dest => dest.Categories, opt => opt.Ignore()); // Optional: Handle separately
+                .ForMember(dest => dest.Categories, opt => opt.Ignore());
 
-            // Map Category <-> CategoryDto
+           
             CreateMap<Category, CategoryDto>().ReverseMap();
             CreateMap<Product, ProductDto>().ReverseMap();
 
-            // Map string <-> Category (useful for manual mapping)
+           
             CreateMap<string, Category>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src));
 
